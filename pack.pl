0adf3286fe8db3e10ff0454a6cd2f8786dfceda9
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2025-05-15 20:57:02 +0200

    v0.13

--- pack.pl
+++ pack.pl
@@ -1,11 +1,11 @@
 name(units).
 title('unit and quantity arithmetic for swi-prolog').
-version('0.12').
+version('0.13').
 author('Kwon-Young Choi', 'kwon-young.choi@hotmail.fr').
 maintainer('Kwon-Young Choi', 'kwon-young.choi@hotmail.fr').
 packager('Kwon-Young Choi', 'kwon-young.choi@hotmail.fr').
 home('https://github.com/kwon-young/units').
 download('https://github.com/kwon-young/units/releases/*.zip').
 provides(units).
-requires(prolog >= '9.3.15').
+requires(prolog >= '9.3.23').
 requires(clpBNR).

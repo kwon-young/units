b1fb8976ca154f0902253191a46ca76d60182f51
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2025-06-08 21:14:19 +0200

    refactor: Move utility predicates to units/utils module

--- prolog/units.pl
+++ prolog/units.pl
@@ -344,6 +344,7 @@ units:dimension_symbol(dim_1, '').
 units:quantity_parent(1, dim_1).
 units:unit_kind(1, 1).
 
+:- use_module(units/utils).
 :- use_module(units/systems/isq).
 :- use_module(units/systems/si).
 :- use_module(units/systems/angular).
@@ -420,131 +421,6 @@ error:has_type(Quantity, Term) :-
       Q = R.q.q
    ),
    implicitly_convertible(Q, Quantity).
-
-normalize(In, Out) :-
-   parse(In, L),
-   normalize_factors(L, L1),
-   generate_expression(L1, Out).
-
-normalize_dimension(In, Out) :-
-   normalize(In, N),
-   (  N == 1
-   -> Out = dim_1
-   ;  Out = N
-   ).
-
-parse(Expr, Factors) :-
-   phrase(parse(1, Expr), Factors).
-
-parse(Coeff, A*B) ==>
-   parse(Coeff, A), parse(Coeff, B).
-parse(Coeff, A/B) ==>
-   parse(Coeff, A),
-   parse(-Coeff, B).
-parse(Coeff, A**N) ==>
-   parse(Coeff*N, A).
-parse(_, dim_1) ==>
-   [].
-parse(Coeff, A) ==>
-   [A-Coeff].
-
-normalize_factors(L, L2) :-
-   msort(L, L1),
-   aggregate(L1, L2).
-
-parse_normalize_factors(In, L3) :-
-   parse(In, L),
-   normalize_factors(L, L3).
-
-aggregate(L, L2) :-
-   group_pairs_by_key(L, Groups),
-   maplist([A-Ns, A-N]>>sum_list(Ns, N), Groups, L1),
-   simplify(L1, L2).
-
-simplify([], R) => R = [].
-simplify([_-0 | T], R) =>
-   simplify(T, R).
-simplify([1-_ | T], R) =>
-   simplify(T, R).
-simplify([H | T], R) =>
-   R = [H | L],
-   simplify(T, L).
-
-generate_expression(In, Out) :-
-   partition(is_num, In, Num, Denom),
-   maplist(power, Num, Num1),
-   phrase(inverse(Denom), Denom1),
-   maplist(power, Denom1, Denom2),
-   num_denom(Num1, Denom2, Out).
-
-is_num(_-N) :- N > 0.
-
-power(A-1, A) :- !.
-power(A-N, A**N).
-
-inverse([]) --> [].
-inverse([A-N | L]) -->
-   { N1 is -N },
-   [A-N1],
-   inverse(L).
-
-num_denom([], Denom, Expr) :-
-   denom(Denom, 1, Expr).
-num_denom([H | T], Denom, Expr) :-
-   multiply([H | T], Num),
-   denom(Denom, Num, Expr).
-
-denom([], Num, Num).
-denom([H | T], Num, Num/Expr) :-
-   multiply([H | T], Expr).
-
-multiply([H | T], Expr) :-
-   foldl([B, A, A*B]>>true, T, H, Expr).
-
-:- meta_predicate mapexpr(1, ?).
-
-mapexpr(Goal, A) :-
-   mapexpr1(Goal, [_]>>true, A).
-
-:- meta_predicate mapexpr1(1, 1, ?).
-
-mapexpr1(Goal, F, A*B) =>
-   mapexpr1(Goal, F, A),
-   mapexpr1(Goal, F, B).
-mapexpr1(Goal, F, A/B) =>
-   mapexpr1(Goal, F, A),
-   mapexpr1(Goal, F, B).
-mapexpr1(Goal, F, A**_) =>
-   mapexpr1(Goal, F, A).
-mapexpr1(Goal, Failure, A) =>
-   (  call(Goal, A)
-   *-> true
-   ;  call(Failure, A)
-   ).
-
-:- meta_predicate mapexpr(2, ?, ?).
-
-mapexpr(Goal, A, R) :-
-   mapexpr(Goal, =, A, R).
-
-:- meta_predicate mapexpr(2, 2, ?, ?).
-
-mapexpr(Goal, F, A*B, R) =>
-   mapexpr(Goal, F, A, A1),
-   mapexpr(Goal, F, B, B1),
-   R = A1*B1.
-mapexpr(Goal, F, A/B, R) =>
-   mapexpr(Goal, F, A, A1),
-   mapexpr(Goal, F, B, B1),
-   R = A1/B1.
-mapexpr(Goal, F, A**B, R) =>
-   mapexpr(Goal, F, A, A1),
-   R = A1**B.
-mapexpr(Goal, Failure, A, A1) =>
-   (  call(Goal, A, A1)
-   *-> true
-   ;  call(Failure, A, A1)
-   ).
 
 :- table common_expr/6.
 
